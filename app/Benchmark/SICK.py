e6c53aafbf3f9ab6392fad6a3dbbe0df2dbdf706
Tobka <tobka777@gmail.com>
2022-01-19 00:23:46 +0100

    add sick de dataset

--- app/Benchmark/SICK.py
+++ app/Benchmark/SICK.py
@@ -5,10 +5,10 @@ import os
 class SICK(BaseBenchmark):
   def load_dataset(self, lang='en'):
     if lang=='de':
-      #train = self.load_dataset_from_file("data/sickbenchmark_de/sick_de-train.csv", 1, 2, 3, ",")
-      #dev = self.load_dataset_from_file("data/sickbenchmark_de/sick_de-dev.csv", 1, 2, 3, ",")
+      train = self.load_dataset_from_file("data/sickbenchmark/de/sick-train.csv", 1, 2, 3, ";")
+      dev = self.load_dataset_from_file("data/sickbenchmark/de/sick-dev.csv", 1, 2, 3, ",")
       test = self.load_dataset_from_file("data/sickbenchmark/de/sick-test.csv", 1, 2, 3, ";")
-      return pd.concat([test])
+      return pd.concat([train, dev, test])
     else:
       train = self.load_dataset_from_file("data/sickbenchmark/en/sick-train.csv", 1, 2, 3, ",")
       dev = self.load_dataset_from_file("data/sickbenchmark/en/sick-dev.csv", 1, 2, 3, ",")
--- app/translate.py
+++ app/translate.py
@@ -4,14 +4,14 @@ from Benchmark import SICK
 
 benchmark = SICK()
 
-data = benchmark.load_dataset_from_file("data/sickbenchmark/en/sick-dev.csv", 1, 2, 3, ",", True)
-#data = benchmark.load_dataset_from_file("data/sickbenchmark/de/sick-dev.csv", 1, 2, 3, ",", True)
+data = benchmark.load_dataset_from_file("data/sickbenchmark/en/sick-train.csv", 1, 2, 3, ",", True)
 
 for i, row in data.iterrows():
   t1 = benchmark.translate(row[1],'DE')
   t2 = benchmark.translate(row[2],'DE')
   data.at[i,1] = t1
   data.at[i,2] = t2
+  with open("Benchmark/data/sickbenchmark/de/sick-train_bak.csv", "a") as f:
+    f.write(data.iloc[[i]].to_csv(sep=";", encoding='utf-8', index=False, header=False))
 
-data.to_csv("Benchmark/data/sickbenchmark/de/dev.csv", sep=";", encoding='utf-8', index=False, header=False)
-
+data.to_csv("Benchmark/data/sickbenchmark/de/sick-train.csv", sep=";", encoding='utf-8', index=False, header=False)
