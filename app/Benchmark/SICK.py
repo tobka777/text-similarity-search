416a76f1ab8b6f5909e7568f8102625621bd7c52
Tobka <tobka777@gmail.com>
2022-01-25 13:34:32 +0100

    update sick

--- app/Benchmark/SICK.py
+++ app/Benchmark/SICK.py
@@ -6,7 +6,7 @@ class SICK(BaseBenchmark):
   def load_dataset(self, lang='en'):
     if lang=='de':
       train = self.load_dataset_from_file("data/sickbenchmark/de/sick-train.csv", 1, 2, 3, ";")
-      dev = self.load_dataset_from_file("data/sickbenchmark/de/sick-dev.csv", 1, 2, 3, ",")
+      dev = self.load_dataset_from_file("data/sickbenchmark/de/sick-dev.csv", 1, 2, 3, ";")
       test = self.load_dataset_from_file("data/sickbenchmark/de/sick-test.csv", 1, 2, 3, ";")
       return pd.concat([train, dev, test])
     else:
